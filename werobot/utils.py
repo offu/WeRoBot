ffbf19e5e295f371119dee83669dff93648d1c05
whtsky <whtsky@me.com>
2013-02-02 19:32:30 +0800

    support py3k..

--- werobot/utils.py
+++ werobot/utils.py
@@ -9,6 +9,13 @@ try:
 except ImportError:
     curses = None
 
+py   = sys.version_info
+py3k = py >= (3,0,0)
+
+if py3k:
+    basestring = str
+    unicode = str
+
 
 def to_unicode(value):
     if isinstance(value, unicode):
