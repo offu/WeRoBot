54163d10d5961873a59a8f5b80c24a12134ed1cc
whtsky <whtsky@me.com>
2013-02-03 20:46:57 +0800

    Support python 3.1 & 3.3

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         ],
