dd9a28a89efe6584ec1255252a41e677492e090b
whtsky <whtsky@me.com>
2013-02-03 17:45:32 +0800

    add tox.ini

--- setup.py
+++ setup.py
@@ -35,7 +35,9 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         ],
+    tests_require=['nose'],
     test_suite='nose.collector',
     **kwargs
 )
