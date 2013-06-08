93198539130a9c5c3883486fa9500dfdfd7fe973
Reorx <novoreorx@gmail.com>
2013-06-09 02:27:05 +0800

    change the way of handler adding and executing on BaseRoBot

--- werobot/robot.py
+++ werobot/robot.py
@@ -2,7 +2,7 @@
 
 import inspect
 import hashlib
-
+import warnings
 from bottle import Bottle, request, response, abort
 
 from .parser import parse_user_msg
@@ -11,84 +11,95 @@ from .reply import create_reply
 __all__ = ['BaseRoBot', 'WeRoBot']
 
 
-class BaseRoBot(object):
-    def __init__(self, token=None):
-        self._handlers = {
-            "subscribe": [],
-            "unsubscribe": [],
-            "click": [],
-            "link": [],
-            "text": [],
-            "image": [],
-            "location": [],
-            "unknown": []
-        }
-        self.token = token
+def fallback_handler(message):
+    return 'Unhandled'
 
-    def handler(self, f):
+
+class BaseRoBot(object):
+    message_types = ['subscribe', 'unsubscribe', 'click'  # event
+                     'text', 'image', 'link', 'location',
+                     'music', 'news']
+
+    def __init__(self, token=None):
+        self.token = token
+        self._handlers = dict((k, None) for k in self.message_types)
+        self._fallback = {'handler': fallback_handler}
+
+    def subscribe(self, f):
         """
-        Decorator to add a new handler to the robot.
+        Decorator to register handler function for ``subscribe event`` messages
         """
-        self.add_handler(f, types=[])
-        return f
+        self.add_handler(f, types=['subscribe'])
+
+    def unsubscribe(self, f):
+        """
+        Decorator to register handler function for ``unsubscribe event`` messages
+        """
+        self.add_handler(f, types=['unsubscribe'])
+
+    def click(self, f):
+        """
+        Decorator to register handler function for ``click event`` messages
+        """
+        self.add_handler(f, types=['click'])
 
     def text(self, f):
         """
-        Decorator to add a new handler to the robot.
+        Decorator to register handler function for ``text`` messages
         """
         self.add_handler(f, types=['text'])
         return f
 
     def image(self, f):
         """
-        Decorator to add a new handler to the robot.
+        Decorator to register handler function for ``image`` messages
         """
         self.add_handler(f, types=['image'])
         return f
 
+    def link(self, f):
+        """
+        Decorator to register handler function for ``link`` messages
+        """
+        self.add_handler(f, types=['link'])
+        return f
+
     def location(self, f):
         """
-        Decorator to add a new handler to the robot.
+        Decorator to register handler function for ``location`` messages
         """
         self.add_handler(f, types=['location'])
         return f
 
-    def link(self, f):
-        self.add_handler(f, types=['link'])
+    def fallback(self, f):
+        """
+        Decorator to register handler function for messages that have no relevant handlers
+        """
+        self._fallback['handler'] = f
         return f
 
-    def subscribe(self, f):
-        self.add_handler(f, types=['subscribe'])
-
-    def unsubscribe(self, f):
-        self.add_handler(f, types=['unsubscribe'])
-
-    def click(self, f):
-        self.add_handler(f, types=['click'])
-
-    def unknown(self, f):
-        """
-        Decorator to add a new handler to the robot.
-        """
-        self.add_handler(f, types=['unknown'])
-        return f
+    def get_fallback_handler(self):
+        return self._fallback['handler']
 
     def add_handler(self, func, types=None):
         """
         Add a new handler to the robot.
         """
-        if not types:
-            types = self._handlers.keys()
+        assert types, 'You should specify one or more types for the handler function'
         if not inspect.isfunction(func):
-            raise TypeError
-        for type in types:
-            self._handlers[type].append(func)
+            raise TypeError('"%s" should be a funciton' % func)
+        for type_ in types:
+            self._handlers[type_] = func
 
     def _get_reply(self, message):
-        for handler in self._handlers[message.type]:
-            reply = handler(message)
-            if reply:
-                return reply
+        if not message.type in self.message_types:
+            warnings.warn('Encounter new message type: %s' % message.type)
+
+        handler = self._handlers[message.type]
+        if handler is None:
+            handler = self.get_fallback_handler()
+
+        return handler(message)
 
     def check_signature(self, timestamp, nonce, signature):
         sign = [self.token, timestamp, nonce]
