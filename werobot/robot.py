a6b53f5c279e2f90936989778abf1507669b425a
whtsky <whtsky@me.com>
2013-07-03 20:14:09 +0800

    change the way of handler adding on BaseRoBot. Based on @reorx  's pull request.

--- werobot/robot.py
+++ werobot/robot.py
@@ -11,83 +11,102 @@ __all__ = ['BaseRoBot', 'WeRoBot']
 
 
 class BaseRoBot(object):
+    message_types = ['subscribe', 'unsubscribe', 'click',  # event
+                     'text', 'image', 'link', 'location']
+
     def __init__(self, token=None):
-        self._handlers = {
-            "subscribe": [],
-            "unsubscribe": [],
-            "click": [],
-            "link": [],
-            "text": [],
-            "image": [],
-            "location": [],
-            "unknown": []
-        }
+        self._handlers = dict((k, []) for k in self.message_types)
+        self._handlers['fallback'] = lambda x, err: None
         self.token = token
 
     def handler(self, f):
         """
-        Decorator to add a new handler to the robot.
+        Decorator to add a handler function for every messages
         """
         self.add_handler(f, types=[])
         return f
 
     def text(self, f):
         """
-        Decorator to add a new handler to the robot.
+        Decorator to add a handler function for ``text`` messages
         """
         self.add_handler(f, types=['text'])
         return f
 
     def image(self, f):
         """
-        Decorator to add a new handler to the robot.
+        Decorator to add a handler function for ``image`` messages
         """
         self.add_handler(f, types=['image'])
         return f
 
     def location(self, f):
         """
-        Decorator to add a new handler to the robot.
+        Decorator to add a handler function for ``location`` messages
         """
         self.add_handler(f, types=['location'])
         return f
 
     def link(self, f):
+        """
+        Decorator to add a handler function for ``link`` messages
+        """
         self.add_handler(f, types=['link'])
         return f
 
     def subscribe(self, f):
+        """
+        Decorator to add a handler function for ``subscribe event`` messages
+        """
         self.add_handler(f, types=['subscribe'])
 
     def unsubscribe(self, f):
+        """
+        Decorator to add a handler function for ``unsubscribe event`` messages
+        """
         self.add_handler(f, types=['unsubscribe'])
 
     def click(self, f):
+        """
+        Decorator to add a handler function for ``click`` messages
+        """
         self.add_handler(f, types=['click'])
 
-    def unknown(self, f):
+    def fallback(self, f):
         """
-        Decorator to add a new handler to the robot.
+        Decorator to register handler function for messages that have no relevant handlers
         """
-        self.add_handler(f, types=['unknown'])
-        return f
+        self.add_handler(f, types=['fallback'])
+
+    def get_fallback_handler(self):
+        return self._handlers['fallback']
 
     def add_handler(self, func, types=None):
         """
-        Add a new handler to the robot.
+        Add a handler function for messages of given types.
         """
         if not types:
             types = self._handlers.keys()
         if not inspect.isfunction(func):
             raise TypeError
-        for type in types:
-            self._handlers[type].append(func)
+        if types == 'fallback':
+            self._handlers['fallback'] = func
+        else:
+            for type in types:
+                self._handlers[type].append(func)
 
-    def _get_reply(self, message):
-        for handler in self._handlers[message.type]:
-            reply = handler(message)
-            if reply:
-                return reply
+    def get_reply(self, message):
+        """
+        Return the raw xml reply for the given message.
+        """
+        try:
+            for handler in self._handlers[message.type]:
+                reply = handler(message)
+                if reply:
+                    return reply
+        except Exception, e:
+            fallback = self.get_fallback_handler()
+            fallback(message, e)
 
     def check_signature(self, timestamp, nonce, signature):
         sign = [self.token, timestamp, nonce]
@@ -128,7 +147,7 @@ class WeRoBot(BaseRoBot):
 
             body = request.body.read()
             message = parse_user_msg(body)
-            reply = self._get_reply(message)
+            reply = self.get_reply(message)
             if not reply:
                 return ''
             response.content_type = 'application/xml'
