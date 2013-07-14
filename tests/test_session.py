2837582820ccf9cc9e7e2cd77dc220f5f7cd335b
whtsky <whtsky@me.com>
2013-07-14 18:56:32 +0800

    remove session file before testing

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,9 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import os
+
 import werobot
 import werobot.utils
 import werobot.testing
 
 
+def remove_file_session():
+    try:
+        os.remove("werobot_session.db")
+    except:
+        pass
+
+
 def test_session():
+    remove_file_session()
     robot = werobot.WeRoBot(token=werobot.utils.generate_token(),
                             enable_session=True)
 
