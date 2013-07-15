d0baa835dfa79380c33766780982b479be6bc5ca
whtsky <whtsky@me.com>
2013-07-16 00:01:59 +0800

    append themes path in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('_themes'))
 
 import werobot
 
