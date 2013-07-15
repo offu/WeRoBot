00f539a5a6d0cbed356efae8d50306e25e6e3c70
whtsky <whtsky@me.com>
2013-07-15 20:56:22 +0800

    add tests for MongoDBStorage and RedisStorage

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 import werobot
 import werobot.utils
 import werobot.testing
+from werobot.session import filestorage, mongodbstorage, redisstorage
+
+import pymongo
+import redis
 
 
-def remove_file_session():
-    try:
-        os.remove("werobot_session.db")
-    except:
-        pass
+def remove_session(session):
+    del session['test']
 
 
 def test_session():
-    remove_file_session()
     robot = werobot.WeRoBot(token=werobot.utils.generate_token(),
                             enable_session=True)
 
@@ -33,4 +31,14 @@ def test_session():
     tester = werobot.testing.WeTest(robot)
     message_1 = werobot.testing.make_text_message('ss')
     message_2 = werobot.testing.make_text_message('dd')
-    assert tester.send(message_1) == tester.send(message_2) == 'ss'
+
+    session_storages = [
+        filestorage.FileStorage(),
+        mongodbstorage.MongoDBStorage(pymongo.MongoClient().t.t),
+        redisstorage.RedisStorage(redis.Redis()),
+    ]
+
+    for session_storage in session_storages:
+        robot.session_storage = session_storages
+        assert tester.send(message_1) == tester.send(message_2) == 'ss'
+        remove_session(session_storage)
