a26d74c08348df37a62b8be67fd8d4e95347e9b5
whtsky <whtsky@gmail.com>
2013-09-18 23:34:00 +0800

    friendly 403 page

--- werobot/robot.py
+++ werobot/robot.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import werobot
+
 import inspect
 import hashlib
 import logging
 
-from bottle import Bottle, request, response, abort
+from bottle import Bottle, request, response, abort, template
 
 from .parser import parse_user_msg
 from .reply import create_reply
@@ -137,6 +141,29 @@ class BaseRoBot(object):
 
 class WeRoBot(BaseRoBot):
 
+    ERROR_PAGE_TEMPLATE = """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf8" />
+            <title>Error: {{e.status}}</title>
+            <style type="text/css">
+              html {background-color: #eee; font-family: sans;}
+              body {background-color: #fff; border: 1px solid #ddd;
+                    padding: 15px; margin: 15px;}
+              pre {background-color: #eee; border: 1px solid #ddd; padding: 5px;}
+            </style>
+        </head>
+        <body>
+            <h1>Error: {{e.status}}</h1>
+            <p>微信机器人不可以通过 GET 方式直接进行访问。</p>
+            <p>想要使用本机器人，请在微信后台中将 URL 设置为 <pre>{{request.url}}</pre> 并将 Token 值设置正确。</p>
+
+            <p>如果你仍有疑问，请<a href="http://werobot.readthedocs.org/en/%s/">阅读文档</a>
+        </body>
+    </html>
+    """ % werobot.__version__
+
     @property
     def wsgi(self):
         if not self._handlers:
@@ -172,6 +199,11 @@ class WeRoBot(BaseRoBot):
                 return ''
             response.content_type = 'application/xml'
             return create_reply(reply, message=message)
+
+        @app.error(403)
+        def error403(error):
+            return template(self.ERROR_PAGE_TEMPLATE,
+                            e=error, request=request)
 
         return app
 
