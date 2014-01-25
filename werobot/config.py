b8430e9c17f2deca79b636473478b93d32eaf50f
whtsky <whtsky@gmail.com>
2014-01-25 14:47:04 +0800

    Add `werobot.config`

--- werobot/config.py
+++ werobot/config.py
@@ -2,4 +2,24 @@
 
 
 class Config(dict):
-    pass
+    def from_pyfile(self, filename):
+        """
+        在一个 Python 文件中读取配置。
+
+        :param filename: 配置文件的文件名。
+        """
+        d = object()
+        with open(filename) as config_file:
+            exec (compile(config_file.read(), filename, 'exec'), d.__dict__)
+        self.from_object(d)
+        return True
+
+    def from_object(self, obj):
+        """
+        在给定的 Python 对象中读取配置。
+
+        :param obj: 一个 Python 对象
+        """
+        for key in dir(obj):
+            if key.isupper():
+                self[key] = getattr(obj, key)
