cbec14292a4b8ce63c2f0b19c044a037eeb65f68
whtsky <whtsky@gmail.com>
2014-02-05 21:04:22 +0800

    Writing tests for to_binary

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@
 
 import six
 
-from werobot.utils import generate_token, check_token, to_text
+from werobot.utils import generate_token, check_token, to_text, to_binary
 
 
 def test_token_generator():
@@ -12,5 +12,17 @@ def test_token_generator():
 
 def test_to_text():
     assert to_text(6) == six.text_type(6)
-    assert to_text(six.binary_type("aa")) == "aa"
-    assert to_text(six.text_type("cc")) == "cc"
+    assert to_text(b"aa") == "aa"
+    assert to_text("cc") == "cc"
+    if six.PY2:
+        assert to_text(u"喵") == u"喵"
+        assert to_text("喵") == u"喵"
+
+
+def test_to_binary():
+    assert to_binary(6) == six.binary_type(6)
+    assert to_binary(b"aa") == b"aa"
+    assert to_binary("cc") == b"cc"
+    if six.PY2:
+        assert to_binary(u"喵") == "喵"
+        assert to_binary("喵") == "喵"
