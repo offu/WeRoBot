705a3baa828a04e801c12d872f8bcbb1b4da2d42
whtsky <whtsky@gmail.com>
2014-07-17 14:07:43 +0800

    Fix FileStorage's bug

--- werobot/session/filestorage.py
+++ werobot/session/filestorage.py
@@ -20,7 +20,10 @@ class FileStorage(SessionStorage):
         self.db = dbm.open(filename, "c")
 
     def get(self, id):
-        session_json = self.db.get(id, "{}")
+        try:
+            session_json = self.db["id"]
+        except KeyError:
+            session_json = "{}"
         return json_loads(session_json)
 
     def set(self, id, value):
