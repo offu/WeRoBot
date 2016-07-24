1798e0cf32be81fd53e5eec21a5d6f975bb803c9
whtsky <me@tiantian.codes>
2016-07-24 21:30:18 +0800

    Attempt to run tox on OS X

--- travis/install.sh
+++ travis/install.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
+brew update
 
-if [ "$(uname)" == "Darwin" ]; then
-  brew update
-  brew install redis
-  brew services start redis
-  brew install mongodb
-  brew services start mongodb
+case $PYTHON_VERSION in
+    system)
+      ;;
+    tox)
+      brew install pyenv
+      eval "$(pyenv init -)"
+      pyenv install 2.6.9
+      pyenv install 2.7.12
+      pyenv install 3.3.6
+      pyenv install 3.4.5
+      pyenv install 3.5.2
+      pyenv install pypy-5.3.1
+      pyenv local 2.6.9 2.7.12 3.3.6 3.4.5 3.5.2 pypy-5.3.1
+      ;;
+    *)
+      source travis/terryfy/library_installers.sh
+      clean_builds
+      get_python_environment $PYTHON_INSTALL_METHOD $PYTHON_VERSION venv
+      ;;
+esac
 
-  source travis/terryfy/library_installers.sh
-  clean_builds
-  get_python_environment $PYTHON_INSTALL_METHOD $PYTHON_VERSION venv
-fi
-
+brew install redis
+brew services start redis
+brew install mongodb
+brew services start mongodb
 pip install --upgrade pip wheel
 pip install -r dev-requirements.txt
+python --version
---
+++ travis/script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ "$PYTHON_VERSION" == "tox" ]; then
+  tox
+else
+  coverage run --source werobot -m py.test
+fi
