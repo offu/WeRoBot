bddc4961904393d31f81afc590dfac6ae2c86813
Yuyang Zhang <695441493@qq.com>
2016-07-26 13:30:44 +0800

    fix pypy in tox on MacOs

--- travis/script.sh
+++ travis/script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $PYTHON_INSTALL_METHOD == "tox" ]; then
-  tox
+  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox
 else
   coverage run --source werobot -m py.test
 fi
