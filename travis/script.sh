0de30e47f142799e337fb34e57d370955e3d460a
Yuyang Zhang <695441493@qq.com>
2016-11-25 18:35:13 +0800

    make travis more elegant

--- travis/script.sh
+++ travis/script.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ $PYTHON_INSTALL_METHOD == "tox" ] && [ $TEST_NUMBER == "1" ]; then
-  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c tox-travis-1.ini
+  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -e $(tox -l | grep -v py3 | tr "\n" ",")
 elif [ $PYTHON_INSTALL_METHOD == "tox" ] && [ $TEST_NUMBER == "2" ]; then
-  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c tox-travis-2.ini
+  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -e $(tox -l | grep py3 | tr "\n" ",")
 else
   python setup.py install
   coverage run --source werobot -m py.test
