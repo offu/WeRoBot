022dd05b4dec124ccdcff830074aa66355f48cb4
Yuyang Zhang <695441493@qq.com>
2016-11-25 16:23:05 +0800

    move tox for travis into root

--- travis/script.sh
+++ travis/script.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-echo $PYTHON_INSTALL_METHOD
-echo $TEST_NUMBER
-
 if [ $PYTHON_INSTALL_METHOD == "tox" ] && [ $TEST_NUMBER == "1" ]; then
-  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c travis/tox-travis-1.ini
+  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c tox-travis-1.ini
 elif [ $PYTHON_INSTALL_METHOD == "tox" ] && [ $TEST_NUMBER == "2" ]; then
-  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c travis/tox-travis-2.ini
+  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c tox-travis-2.ini
 else
   python setup.py install
   coverage run --source werobot -m py.test
