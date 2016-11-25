5bacd706f2bbb5c6bb960d81d5323c939bb96920
Yuyang Zhang <695441493@qq.com>
2016-11-25 15:44:32 +0800

    try to fix again

--- travis/script.sh
+++ travis/script.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-if [ $PYTHON_INSTALL_METHOD == "tox" ]; then
-  if [ $TEST_NUMBER == "1" ]; then
-    env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c travis/tox-travis-1.ini
-  else
-    env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c travis/tox-travis-2.ini
-  fi
+echo $PYTHON_INSTALL_METHOD
+echo $TEST_NUMBER
+
+if [ $PYTHON_INSTALL_METHOD == "tox" ] && [ $TEST_NUMBER == "1" ]; then
+  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c travis/tox-travis-1.ini
+elif [ $PYTHON_INSTALL_METHOD == "tox" ] && [ $TEST_NUMBER == "2" ]; then
+  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c travis/tox-travis-2.ini
 else
   python setup.py install
   coverage run --source werobot -m py.test
