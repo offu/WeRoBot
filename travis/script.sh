2a867edab659862eb3a5cf3c27858476844b7a26
Yuyang Zhang <695441493@qq.com>
2016-11-25 15:20:21 +0800

    split test with tox on travis #173

--- travis/script.sh
+++ travis/script.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 if [ $PYTHON_INSTALL_METHOD == "tox" ]; then
-  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox
+  if [ $TEST_NUMBER == "1" ]; then
+    env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c travis/tox-travis-1.ini
+  else
+    env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -c travis/tox-travis-2.ini
 else
   python setup.py install
   coverage run --source werobot -m py.test
