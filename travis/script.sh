86a83b61ca623d4e23ec606fb177df581adfd439
Yuyang Zhang <695441493@qq.com>
2016-11-26 10:39:12 +0800

    fix script again

--- travis/script.sh
+++ travis/script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [ $PYTHON_INSTALL_METHOD == "tox" ] && [ $PYTHON_VERSION == "pypy-5.0.0 2.7.12" ]; then
+if [ $PYTHON_INSTALL_METHOD == "tox" ] && [ "$PYTHON_VERSION" == "pypy-5.0.0 2.7.12" ]; then
   env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -e $(tox -l | grep -v py3 | tr "\n" ",")
-elif [ $PYTHON_INSTALL_METHOD == "tox" ] && [ $PYTHON_VERSION == "3.5.2 3.4.5" ]; then
+elif [ $PYTHON_INSTALL_METHOD == "tox" ] && [ "$PYTHON_VERSION" == "3.5.2 3.4.5" ]; then
   env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -e $(tox -l | grep py3 | tr "\n" ",")
 else
   python setup.py install
