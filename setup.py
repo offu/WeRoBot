925d7dab8a345b097b7e188cd10ce158899b9d67
Yuyang Zhang <695441493@qq.com>
2017-01-09 18:14:36 +0800

    add python3.6 in setup.py

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Intended Audience :: Developers',
