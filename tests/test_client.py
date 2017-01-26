0b00b6dd3b8f6dc02c1a14474147d6874a1cf7a7
Yuyang Zhang <695441493@qq.com>
2017-01-26 14:46:16 +0800

    fix test

--- tests/test_client.py
+++ tests/test_client.py
@@ -15,30 +15,30 @@ menu_data = {
     "button": [
         {
             "type": "click",
-            "name": "今日歌曲",
+            "name": u"今日歌曲",
             "key": "V1001_TODAY_MUSIC"
         },
         {
             "type": "click",
-            "name": "歌手简介",
+            "name": u"歌手简介",
             "key": "V1001_TODAY_SINGER"
         },
         {
-            "name": "菜单",
+            "name": u"菜单",
             "sub_button": [
                 {
                     "type": "view",
-                    "name": "搜索",
+                    "name": u"搜索",
                     "url": "http://www.soso.com/"
                 },
                 {
                     "type": "view",
-                    "name": "视频",
+                    "name": u"视频",
                     "url": "http://v.qq.com/"
                 },
                 {
                     "type": "click",
-                    "name": "赞一下我们",
+                    "name": u"赞一下我们",
                     "key": "V1001_GOOD"
                 }
             ]
