866f6b00ac64f6e957dc26042c1b2f21d883a3b8
Wu Haotian <whtsky@gmail.com>
2017-01-29 21:45:22 +0800

    split pypy tox tests on Travis

--- travis/script.sh
+++ travis/script.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-if [ $PYTHON_INSTALL_METHOD == "tox" ] && [ $PYTHON_MAJOR == "2" ]; then
-  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -e $(tox -l | grep -v py3 | tr "\n" ",")
-elif [ $PYTHON_INSTALL_METHOD == "tox" ] && [ $PYTHON_MAJOR == "3" ]; then
-  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -e $(tox -l | grep py3 | tr "\n" ",")
+if [ $PYTHON_INSTALL_METHOD == "tox" ] then
+  env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -e $(tox -l | grep $PYTHON_MAJOR | tr "\n" ",")
 else
   python setup.py install
   coverage run --source werobot -m py.test
