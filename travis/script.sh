17476e5d178769cb6770b722a7ddf4660de01155
Yuyang Zhang <helloqiu95@gmail.com>
2017-09-17 09:45:20 +0800

    fix travis

--- travis/script.sh
+++ travis/script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 brew services list
-env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -e $(tox -l | grep $PYTHON_MAJOR | tr "\n" ",")
+env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" tox -e $(tox -l | grep -E $PYTHON_MAJOR | tr "\n" ",")
