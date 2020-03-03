0cc9bdf19e46f9d84a58232e0243a87cf67ac97d
mywaiting <ourwaiting@gmail.com>
2020-03-03 20:27:34 +0800

    add template_send_job_finish as decorator function to BaseRoBot

--- werobot/robot.py
+++ werobot/robot.py
@@ -79,7 +79,8 @@ class BaseRoBot(object):
         'update_member_card_event',
         'card_sku_remind_event',
         'card_pay_order_event',
-        'submit_membercard_user_info_event',  # event
+        'submit_membercard_user_info_event',  
+        'template_send_job_finish_event', # event
         'text',
         'image',
         'link',
@@ -453,6 +454,12 @@ class BaseRoBot(object):
         事件添加一个 handler 方法的装饰器。
         """
         self.add_handler(f, type='submit_membercard_user_info_event')
+        return f
+
+    def template_send_job_finish(self, f):
+        """在模版消息发送任务完成后，微信服务器会将是否送达成功作为通知，发送到开发者中心中填写的服务器配置地址中
+        """
+        self.add_handler(f, type='template_send_job_finish_event')
         return f
 
     def unknown_event(self, f):
