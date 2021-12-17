3e6cc69660723180f268b94b878c7b42b304a263
klmmlk <klmmlk@users.noreply.github.com>
2021-12-17 16:24:51 +0800

    add fastapi view

---
+++ werobot/contrib/fastApiView.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3.8
+# -*- coding: utf-8 -*-
+import asyncio
+from fastapi import Request,Response
+from fastapi.responses import HTMLResponse
+import html
+
+def make_view(robot):
+    """
+    为一个 BaseRoBot 生成 fastapi view。
+    :param robot:
+    :return:
+    """
+    def werobot_view(request: Request):
+        timestamp = request.query_params.get('timestamp')
+        nonce = request.query_params.get('nonce')
+        signature = request.query_params.get('signature')
+        if not robot.check_signature(
+                timestamp,
+                nonce,
+                signature,
+        ):
+            return HTMLResponse(robot.make_error_page(html.escape(request.url.hostname)), 403)
+        message = robot.parse_message(
+            asyncio.run(request.body()),
+            timestamp=timestamp,
+            nonce=nonce,
+            msg_signature=request.query_params.get('msg_signature', '')
+        )
+        response = Response(robot.get_encrypted_reply(message))
+        response.headers['content_type'] = 'application/xml'
+        return response
+
+    return werobot_view
